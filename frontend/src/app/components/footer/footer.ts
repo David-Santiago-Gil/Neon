0ca249b6f41020f5040daee0ff7f51eee5b3f65a
David-Santiago-Gil <gilsantiagodepa@gmail.com>
2026-04-12 18:20:20 -0500

    Interceptar links vacios en el footer para disparar funciones interactivas simuladas

--- frontend/src/app/components/footer/footer.ts
+++ frontend/src/app/components/footer/footer.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class FooterComponent { }
+export class FooterComponent {
+  
+  simularCarga(juego: string, event: Event) {
+    event.preventDefault();
+    alert(`🎮 Cargando recurso inmersivo: ${juego}`);
+  }
+
+  mostrarPromocion(promo: string, event: Event) {
+    event.preventDefault();
+    alert(`💎 Has descubierto la promoción oculta: ${promo}! Entrando a la zona Elite...`);
+  }
+
+  redireccionDummy(sitio: string, event: Event) {
+    event.preventDefault();
+    alert(`🔌 Conectando a los servidores de ${sitio}... (Sistemas activos)`);
+  }
+}
