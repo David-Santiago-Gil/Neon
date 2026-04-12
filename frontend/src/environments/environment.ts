73c1f11b1235e9b13c037368418825daf67ac2f4
David-Santiago-Gil <gilsantiagodepa@gmail.com>
2026-04-12 17:36:38 -0500

    Agregar /api al url del endpoint the Railway en production

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://TU-BACKEND.up.railway.app/api' // NOTA: Reemplazar con el dominio real de Railway
+  apiUrl: 'https://neon-production-fda1.up.railway.app/api' // NOTA: Reemplazar con el dominio real de Railway
 };
